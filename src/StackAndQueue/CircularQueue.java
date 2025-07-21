5642f29ca342ea099c4158a0c07243ba3afd5e7d
Swapnil Kumar <swapnilkm03@gmail.com>
2025-07-21 23:13:43 +0530

    circular queue

--- src/StackAndQueue/CircularQueue.java
+++ src/StackAndQueue/CircularQueue.java
@@ -2,7 +2,7 @@ package StackAndQueue;
 
 public class CircularQueue {
     protected int[] data;
-
+    private int size=0;
     private static final int DEFAULT_SIZE = 10;
 
     public CircularQueue(){
@@ -16,10 +16,31 @@ public class CircularQueue {
     protected int front=0;
 
     public boolean isFull() {
-        return end==data.length;
+        return size==data.length;
     }
 
     public boolean isEmpty() {
         return end==0;
+    }
+
+    public boolean insert(int item){
+        if(isFull()){
+            return false;
+        }
+
+        data[end]=item;
+        end=end% data.length;
+        size++;
+        return true;
+    }
+
+    public int remove(){
+        if(isEmpty()){
+            return -1;
+        }
+        int removed=data[front++];
+        front=front% data.length;
+        size--;
+        return removed;
     }
 }
